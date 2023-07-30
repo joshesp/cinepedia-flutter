9f43ec81ece992b65853a61383a2eb696c40dcf0
Josh <josh@mac>
2023-07-30 15:44:52 -0600

    modelo de actor

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -40,9 +40,81 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
           physics: const ClampingScrollPhysics(),
           slivers: [
             _CustomSliverAppBar(movie: movie),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => _MovieDetail(movie: movie),
+                childCount: 1,
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _MovieDetail extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieDetail({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(22),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(width: 8),
+              SizedBox(
+                width: (size.width - 46) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyles.titleLarge,
+                    ),
+                    Text(movie.overview),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map(
+                (gender) => Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  child: Chip(
+                    label: Text(gender),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
