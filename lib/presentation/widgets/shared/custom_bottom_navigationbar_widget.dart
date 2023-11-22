0566314d2cc6b346bf5cd19ff92b72738ad0c72f
Josh <josh@imac.com>
2023-11-21 19:06:17 -0600

    configuración de shell router

--- lib/presentation/widgets/shared/custom_bottom_navigationbar_widget.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigationbar_widget.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
+
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).location;
+
+    switch (location) {
+      case '/favorites':
+        return 2;
+      case '/categories':
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 1:
+        // TODO: Update route to categories
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+      default:
+        context.go('/');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (index) => onItemTapped(context, index),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
